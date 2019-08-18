3e2b047c98cbc28afc98ebdca3c20d29150aea3b
Julia Haraieva <y.haraieva@gmail.com>
2019-08-18 21:59:10 +0300

    Fixed LocalDateTime deserializing

--- src/main/java/com/haraieva/bank/controller/TransactionController.java
+++ src/main/java/com/haraieva/bank/controller/TransactionController.java
@@ -1,17 +1,15 @@
 package com.haraieva.bank.controller;
 
-import com.haraieva.bank.dto.TransactionDto;
 import com.haraieva.bank.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Controller
 @RequestMapping("/transactions")
@@ -26,8 +24,10 @@ public class TransactionController {
 
     @GetMapping
     public String getTransactions(Model model, @RequestParam(value = "accountId", required = false) Long accountId,
-                                  @RequestParam(value = "from", required = false) LocalDateTime from,
-                                  @RequestParam(value = "to", required = false) LocalDateTime to) {
+                                  @RequestParam(value = "from", required = false)
+                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+                                  @RequestParam(value = "to", required = false)
+                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
         if (accountId != null) {
             model.addAttribute("transactions", service.findAllByRecipientOrSender(accountId));
         } else if (from != null && to != null) {
